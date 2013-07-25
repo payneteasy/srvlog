d299bcbcdb762e9bd859fea042dbbb22522b4805
Vyacheslav Zarubin <slava.zarubin@gmail.com>
2013-07-25 12:21:30 +0400

    Corrected name of the ServerLog4JEvent class

--- srvlog-service/src/main/java/com/payneteasy/srvlog/adapter/log4j/ServerLog4JEvent.java
+++ srvlog-service/src/main/java/com/payneteasy/srvlog/adapter/log4j/ServerLog4JEvent.java
@@ -8,11 +8,11 @@ import java.net.InetAddress;
  * Date: 19.06.13
  * Time: 20:49
  */
-public class ServerLog4jEvent {
+public class ServerLog4JEvent {
     private final LoggingEvent logEvent;
     private final InetAddress host;
 
-    public ServerLog4jEvent(LoggingEvent logEvent, InetAddress host) {
+    public ServerLog4JEvent(LoggingEvent logEvent, InetAddress host) {
         this.logEvent = logEvent;
         this.host = host;
     }
