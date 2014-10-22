ddd4d9efa85643b9469305ebf2c458de3fc8009c
Artem <imenem@inbox.ru>
2014-10-22 13:24:11 +0300

    GUI for HTTP headers implemented.

--- srvlog-web/src/main/java/com/payneteasy/srvlog/wicket/page/SnortLogMonitorPage.java
+++ srvlog-web/src/main/java/com/payneteasy/srvlog/wicket/page/SnortLogMonitorPage.java
@@ -1,5 +1,6 @@
 package com.payneteasy.srvlog.wicket.page;
 
+import static com.payneteasy.srvlog.adapter.syslog.IpHeader.isContainsIpHeader;
 import com.payneteasy.srvlog.data.LogData;
 import com.payneteasy.srvlog.data.LogFacility;
 import com.payneteasy.srvlog.data.LogLevel;
@@ -7,6 +8,7 @@ import com.payneteasy.srvlog.data.SnortLogData;
 import com.payneteasy.srvlog.service.ILogCollector;
 import static java.lang.String.valueOf;
 import java.util.List;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.apache.commons.lang3.time.DateFormatUtils.format;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -79,15 +81,45 @@ public class SnortLogMonitorPage extends BasePage {
                 li.add(new Label("priority", valueOf(snortLogData.getPriority())));
                 li.add(new Label("classification", snortLogData.getClassification()));
                 li.add(new Label("alert-cause", snortLogData.getAlertCause()));
-                li.add(new Label("header", getHeaderInfo(snortLogData)));
+                li.add(new Label("ip-header", getHeaderInfo(snortLogData)));
                 li.add(new Label("payload", snortLogData.getPayload()));
+
+                addHttpHeaders(li, snortLogData);
+            }
+
+            protected void addHttpHeaders(ListItem<SnortLogData> li, SnortLogData data) {
+                if (!isEmpty(data.getHost())) {
+                    li.add(new Label("host", "Host: " + data.getHost()));
+                }
+                else {
+                    li.add(new Label("host", ""));
+                }
+
+                if (!isEmpty(data.getxForwardedFor())) {
+                    li.add(new Label("x-forwarded-for", "X-Forwarded-For: " + data.getxForwardedFor()  ));
+                }
+                else {
+                    li.add(new Label("x-forwarded-for", ""));
+                }
+
+                if (!isEmpty(data.getxRealIp())) {
+                    li.add(new Label("x-real-ip", "X-Real-IP: " + data.getxRealIp()));
+                }
+                else {
+                    li.add(new Label("x-real-ip", ""));
+                }
             }
 
             private String getHeaderInfo(SnortLogData data) {
+                if (!isContainsIpHeader(data)) {
+                    return "";
+                }
+
                 return data.getProtocolAlias() + " " +
                     data.getSourceIp() + ":" + data.getSourcePort() + " -> " +
                     data.getDestinationIp() + ":" + data.getDestinationPort();
             }
+
         });
 
         add(new WebMarkupContainer("no-snort-logs"){
