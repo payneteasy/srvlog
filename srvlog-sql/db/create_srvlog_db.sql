d5c11ccecd053d03a43403201241080800a215a0
vzarubin <slava.zarubin@gmail.com>
2018-01-15 13:27:25 +0300

    Corrected scripts for travis

--- srvlog-sql/db/create_srvlog_db.sql
+++ srvlog-sql/db/create_srvlog_db.sql
@@ -5,5 +5,3 @@ create database `srvlog` default character set utf8 collate utf8_general_ci;
 grant all privileges on srvlog.* to 'srvlog'@'localhost' identified by '123srvlog123' with grant option;
 
 grant grant option on srvlog.* to 'srvlog'@'localhost';
-
-set global log_bin_trust_function_creators=0;
