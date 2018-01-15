9186889a2bab018ab454049368edc861a301cc84
vzarubin <slava.zarubin@gmail.com>
2018-01-15 11:19:08 +0300

    Corrected scripts for travis

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cd "$(dirname "$0")"
+
 mvn install:install-file -DgroupId=org.sphx.api -DartifactId=sphinx-api -Dpackaging=jar -Dversion=2.0.6 -Dfile=sphinxapi.jar  -DgeneratePom=true
 
 mvn install:install-file -DgroupId=com.nesscomputing -DartifactId=ness-syslog4j -Dpackaging=jar -Dversion=0.9.47-NESS-8-SNAPSHOT -Dfile=ness-syslog4j-0.9.47-NESS-8-SNAPSHOT.jar -DpomFile=ness-syslog4j-0.9.47-NESS-8-SNAPSHOT.pom
