758cfd8bd6536beea05d18ecd6ad39c49a4a4d85
vzarubin <slava.zarubin@gmail.com>
2018-03-30 13:29:30 +0300

    Added ShowVersionServlet and align spring security and springframework version to make them compatible

--- srvlog-web/src/main/java/com/payneteasy/srvlog/ShowVersionServlet.java
+++ srvlog-web/src/main/java/com/payneteasy/srvlog/ShowVersionServlet.java
@@ -2,51 +2,52 @@ package com.payneteasy.srvlog;
 
 import com.payneteasy.srvlog.adapter.syslog.ISyslogAdapterConfig;
 import com.payneteasy.srvlog.service.ILogCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Date: 25.02.13 Time: 16:56
  */
 public class ShowVersionServlet extends HttpServlet{
-    
-    private ISyslogAdapterConfig syslogConfig;
-    private final ReentrantLock lock = new ReentrantLock();
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(ShowVersionServlet.class);
+
+    private volatile String version = "no init";
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        StringBuilder stringBuilder = new StringBuilder();
-
-        ISyslogAdapterConfig syslogAdapterConfig = getSyslogConfig();
-
-        stringBuilder.append("Syslog4j started on  port = ");
-        stringBuilder.append(syslogAdapterConfig.getSyslogPort());
-        stringBuilder.append(" by protocol = ");
-        stringBuilder.append(syslogAdapterConfig.getSyslogProtocol());
-
-        response.getWriter().print(stringBuilder);
-
+        response.setContentType("text/plain");
+        response.getWriter().print(version);
     }
-    
-    private ISyslogAdapterConfig getSyslogConfig(){
-          if(syslogConfig == null){
-            try {
-                lock.lock();
-                ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-                syslogConfig = (ISyslogAdapterConfig) context.getBean("springSyslogAdapterConfig");
-            }finally {
-                lock.unlock();
-            }
-          }
-        return syslogConfig;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        Properties properties = new Properties();
+        InputStream in = getClass().getResourceAsStream("/META-INF/maven/com.payneteasy.srvlog/srvlog-web/pom.properties");
+        if(in == null) {
+            version = "no input stream";
+            return;
+        }
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            version = e.getMessage();
+            LOG.error("Cannot read pom.properties");
+        }
+        version = properties.getProperty("version", "no version");
+        super.init(config);
     }
 }
