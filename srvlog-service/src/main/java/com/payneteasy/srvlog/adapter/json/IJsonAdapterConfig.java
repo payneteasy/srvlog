83bf0023cfcd76c401f955577a4e27996e9c207c
akorobov <akorobov@payneteasy.com>
2024-02-15 18:38:40 +0300

    #78 - json adapter token mask variable

--- srvlog-service/src/main/java/com/payneteasy/srvlog/adapter/json/IJsonAdapterConfig.java
+++ srvlog-service/src/main/java/com/payneteasy/srvlog/adapter/json/IJsonAdapterConfig.java
@@ -10,7 +10,7 @@ public interface IJsonAdapterConfig {
     @AStartupParameter(name = "JSON_ADAPTER_BIND_ADDRESS", value = "127.0.0.1")
     String getWebServerBindAddress();
 
-    @AStartupParameter(name = "JSON_ADAPTER_TOKEN", value = "test-token-197dc68c-34e5-11eb-9fbd-7bb165d4566c")
+    @AStartupParameter(name = "JSON_ADAPTER_TOKEN", value = "test-token-197dc68c-34e5-11eb-9fbd-7bb165d4566c", maskVariable = true)
     String getToken();
 
     @AStartupParameter(name = "JSON_ADAPTER_PATH", value = "/save-logs")
